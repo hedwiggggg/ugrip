66cc5494b5fdb3e6b308288ff75c6d8f9a179526
Silas Rosenkranz <silas.rosenkranz@student.uni-siegen.de>
2020-02-28 16:24:28 +0100

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useCallback } from 'react';
+import generatePdf from './lib/generate-pdf';
+
 import './App.css';
 
+function formatChords(chords) {  
+  let formattedChords = chords;
+  
+  formattedChords = formattedChords.replace(/\[ch\]/g, '<b>');
+  formattedChords = formattedChords.replace(/\[\/ch\]/g, '</b>');
+
+  formattedChords = formattedChords.replace(/\[tab\]/g, '<div>');
+  formattedChords = formattedChords.replace(/\[\/tab\]/g, '</div>');
+
+  return { __html: formattedChords };
+}
+
 function App() {
+  const [uri, setUri] = useState('https://tabs.ultimate-guitar.com/tab/chris-renzema/youre-the-only-one-chords-2709621');
+
+  const [chords, setChords] = useState('paste a ultimate-guitar.com link and press `Load Song`..');
+  const [artist, setArtist] = useState('');
+  const [song, setSong] = useState('');
+
+  const renderChords = useCallback(() => formatChords(chords), [chords]);
+
+  const loadSong = useCallback(() => {
+    fetch(`https://cors-anywhere.glitch.me/${uri}`)
+    .then(res => res.text())
+    .then(text => {
+      const div = document.createElement('div');
+      div.innerHTML = text;
+      
+      const [store] = div.getElementsByClassName('js-store');
+      const storeData = store.getAttribute('data-content');
+
+      const { tab, tab_view } = JSON.parse(storeData).store.page.data;
+    
+      const { song_name, artist_name } = tab;
+      const chords = tab_view.wiki_tab.content;
+    
+      setArtist(artist_name);
+      setSong(song_name);
+      setChords(chords);
+    });
+  }, [uri]);
+
+  const donwloadPdf = useCallback(() => generatePdf(artist, song, chords), [artist, song, chords]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="controls">
+        <input type="text" value={uri} onChange={e => setUri(e.target.value)} />
+        <button onClick={loadSong}>Load Song</button>
+        <button onClick={donwloadPdf}>Download PDF</button>
+      </div>
+
+      <div className="sheet">
+        <div className="artist">{ artist }</div>
+        <div className="song">{ song }</div>
+        <div className="chords" dangerouslySetInnerHTML={renderChords(chords)}></div>
+      </div>
+    </>
   );
 }
 
---
+++ src/lib/generate-pdf.js
@@ -0,0 +1,88 @@
+import pdfMake from 'pdfmake/build/pdfmake';
+import vfsFonts from './vfs_fonts';
+
+pdfMake.vfs = vfsFonts.pdfMake.vfs;
+
+pdfMake.fonts = {
+  'Roboto Mono': {
+    normal: 'RobotoMono-Regular.ttf',
+    bold: 'RobotoMono-Bold.ttf',
+  },
+};
+
+const isOdd = i => i % 2 === 1;
+
+function processChords(chords) {
+  let processedChords = [];
+
+  const tabs = chords.split(/\[tab\]|\[\/tab\]/g);
+  const tabsAndChords = tabs.map(w => w.split(/\[ch\]|\[\/ch\]/g))
+
+  console.log(tabsAndChords);
+  
+
+  for (let i = 0; i < tabsAndChords.length; i++) {
+    const tabAndChords = tabsAndChords[i];
+    let line;
+
+    if (Array.isArray(tabAndChords)) {
+      let inline = [];
+
+      for (let j = 0; j < tabAndChords.length; j++) {
+        const chord = tabAndChords[j];
+        
+        if (isOdd(j)) {
+          inline.push({ text: chord, bold: true });
+        } else {
+          inline.push(chord);
+        }
+      }
+
+      line = { text: inline };
+    } else {
+      line = tabAndChords;
+    }
+
+    processedChords.push(line);
+  }
+    
+  return processedChords;
+}
+
+export default (artist, song, chords) => {
+  const fileName = `chords_${artist}_${song}`;
+  const fileNameFormatted = fileName.replace(/\W/g, '-').toLocaleLowerCase();
+
+  const docDefinition = {
+    content: [
+      { text: artist, style: 'artist' },
+      { text: song, style: 'song' },
+      ' ',
+      ...processChords(chords)
+    ],
+
+    defaultStyle: {
+      font: 'Roboto Mono',
+      fontSize: 12
+    },
+
+    styles: {
+      artist: {
+        fontSize: 16,
+        bold: true
+      },
+      song: {
+        fontSize: 14
+      }
+    },
+
+    pageBreakBefore: (currentNode, followingNodesOnPage, nodesOnNextPage, previousNodesOnPage) => {
+      const isLastOnPage = followingNodesOnPage.length === 0;
+      const isNotLastOfAll = nodesOnNextPage.length !== 0;
+
+      return isLastOnPage && isNotLastOfAll && Array.isArray(currentNode.text);
+    }
+  };
+
+  pdfMake.createPdf(docDefinition).download(fileNameFormatted);
+};
