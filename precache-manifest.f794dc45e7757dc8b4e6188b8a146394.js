669b5f7923015d8c1c4b996fd10fd7a248ec2f43
Silas Rosenkranz <silas.rosenkranz@student.uni-siegen.de>
2020-07-31 10:48:47 +0200

    Updates

---
+++ precache-manifest.f794dc45e7757dc8b4e6188b8a146394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "014a48a3c9ba4c3cd33ca97cee795bcc",
+    "url": "/ugrip/index.html"
+  },
+  {
+    "revision": "ad4157256480f0c00d22",
+    "url": "/ugrip/static/css/main.f043d38e.chunk.css"
+  },
+  {
+    "revision": "698c2d28e22c3ba5cc0c",
+    "url": "/ugrip/static/js/2.d9d7fc45.chunk.js"
+  },
+  {
+    "revision": "48593389b65d672e679678a3e1fe83a5",
+    "url": "/ugrip/static/js/2.d9d7fc45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad4157256480f0c00d22",
+    "url": "/ugrip/static/js/main.9947a50b.chunk.js"
+  },
+  {
+    "revision": "22b5080bc89fe3b0b2b8",
+    "url": "/ugrip/static/js/runtime-main.2dcc412b.js"
+  }
+]);
